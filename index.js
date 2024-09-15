36ddba5a0fe8768fe46f6f77a8f7346d089aae92
Arumuganesan <157499082+Arumuganesan@users.noreply.github.com>
2024-09-15 23:31:50 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+<script>
+    function one(){
+      var cons =  document.getElementById('x').value  += 1
+    }
+    function two(){
+      var cons =  document.getElementById('x').value  += 2
+    }
+    function thr(){
+      var cons =  document.getElementById('x').value  += 3
+    }
+    function four(){
+        document.getElementById('x').value  += 4
+    }
+    function five(){
+        document.getElementById('x').value  += 5
+    }
+    function six(){
+      document.getElementById('x').value  += 6
+    }
+    function sev(){
+       document.getElementById('x').value  += 7
+    }
+    function eig(){
+       document.getElementById('x').value  += 8
+    }
+    function nine(){
+       document.getElementById('x').value  += 9
+    }
+    function zero(){
+       document.getElementById('x').value  += 0
+    }
+    function dzero(){
+       document.getElementById('x').value  +=0
+       document.getElementById('x').value  +=0
+    }
+    function plus(){
+        document.getElementById('x').value  += '+'
+    }
+    function clc(){
+        document.getElementById('x').value = ""
+    }
+    function minus(){
+        document.getElementById('x').value  += '-'
+    }
+    function mul(){
+        document.getElementById('x').value  += '*'
+    }
+    function div(){
+        document.getElementById('x').value  += '/'
+    }
+    function mod(){
+        document.getElementById('x').value  += '%'
+    }
+    function dot(){
+        document.getElementById('x').value  += '.'
+    }
+    function equ(){
+      a = document.getElementById('x').value
+      document.getElementById('x').value = eval(a)
+    }
+    function cross(){
+        b = document.getElementById('x').value *= -1;
+        
+        
+        
+    }
+
+</script>
